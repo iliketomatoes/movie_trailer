57703e608934a0a5042ffd9b0680e9255eae829c
iliketomatoes <giancarlosoverini@gmail.com>
2015-07-01 23:19:51 +0200

    refactor: Define all Video instance properties inside constructor

--- video.py
+++ video.py
@@ -2,18 +2,20 @@ import urllib
 import json
 
 class Video():
+    '''This class provide a way to retrieve movie related data from omdapi'''
 
     def __init__(self, title, youtube_trailer):
         self.title = title
         self.trailer_youtube_url = youtube_trailer
-        self.findRemoteData()
-
-    def findRemoteData(self):
+        #Let's get some additional info, i.e. year, rating and plot
+        additional_data = self.find_remote_data()
+        self.year = additional_data['Year']
+        self.rating = additional_data['imdbRating']
+        self.plot = additional_data['Plot']
+            
+    def find_remote_data(self):
+        '''Fetch year, rating and plot of each movie from http://www.omdbapi.com/'''
         params = urllib.urlencode({'t': self.title, 'plot': 'short', 'r': 'json'})
         response = urllib.urlopen("http://www.omdbapi.com/?%s" % params)
         output = json.load(response)
-        self.year = output['Year']
-        self.rating = output['imdbRating']
-        self.plot = output['Plot']
-        print output
-    
+        return output
